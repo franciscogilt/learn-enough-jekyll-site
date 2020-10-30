ecf4d2af3255c75910a19ef19cb2039304d79f29
franciscogilt <franciscogilt@gmail.com>
2020-10-30 09:22:05 -0400

    Finished Gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,38 @@
+// Activates the image gallery.
+// THe main task is to attach an avent listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function (thumbnail) {
+
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener("click", function () {
+      // Set clicked image as main image.
+      let newImageAlt = thumbnail.dataset.alt;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Change which image is current.
+      let current = document
+        .querySelector(".current")
+        .classList.remove("current");
+
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image description.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
